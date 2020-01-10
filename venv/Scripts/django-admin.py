1a56054b7d5619e3b2d9944d4bac5804b90c3163
Kunal3143 <kunal.s@fnvmart.com>
2020-01-10 19:44:52 +0530

    dashboard

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kits\pycharmprojects\sample\project3\project3\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
